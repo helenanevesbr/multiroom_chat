ed20d0ea26883a64283a6caf6272ed752ce1538d
Helena Neves <helenabrn13@gmail.com>
2022-07-07 19:23:41 -0300

    Validando os dados do formulário de entrada

--- config/server.js
+++ config/server.js
@@ -8,10 +8,13 @@ var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+// configurar como middleware o xpress.static
 app.use(express.static('./app/public'));
 
+// configurar como middleware o body parser
 app.use(bodyParser.urlencoded({ extended: true}));
 
+// configurar como middleware o express validator
 app.use(expressValidator());
 
 load({cwd:'app'})
