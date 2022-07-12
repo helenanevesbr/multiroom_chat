7ab4a6f483aa46dfe0f93e1130bba739b7948edc
Helena Neves <helenabrn13@gmail.com>
2022-07-11 21:08:17 -0300

    Multiroom finalizado

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
 var app = require('./config/server');
 
-app.listen(80, function(){
-   console.log("servidor online")
+var server = app.listen(80, function(){
+   console.log("servidor online");
+});
+
+var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+io.on('connection', function(socket){
+   console.log("Usuario conectou");
+
+   socket.on('disconnect', function(){
+      console.log('Usuario desconectou')
+   });
+
+   socket.on('msgParaServidor', function(data){
+
+      /* dialogo */
+      socket.emit(
+         'msgParaCliente',
+         {apelido: data.apelido, mensagem: data.mensagem}
+         );
+
+      socket.broadcast.emit(
+         'msgParaCliente',
+         {apelido: data.apelido, mensagem: data.mensagem}
+      );
+
+      /* participantes */
+      if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+         socket.emit(
+            'participantesParaCliente',
+            {apelido: data.apelido}
+            );
+   
+         socket.broadcast.emit(
+            'participantesParaCliente',
+            {apelido: data.apelido}
+         );
+      }
+
+   });
+
 });
